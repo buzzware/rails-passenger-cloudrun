57ab7dc959b9894852790aafb3f99b1dde5d2d59
Gary McGhee <gary@buzzware.com.au>
2020-11-15 16:17:45 +0800

    nginx finally booting on given port

--- webapp/docker_start.sh
+++ webapp/docker_start.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env sh
+# see https://stackoverflow.com/questions/40574589/why-does-container-doest-execute-scripts-inside-etc-my-init-d-on-startup/40595636
 /usr/bin/envsubst < /home/app/webapp/nginx.tmpl > /etc/nginx/sites-enabled/nginx.conf || cat /etc/nginx/sites-enabled/nginx.conf
+exit 0
