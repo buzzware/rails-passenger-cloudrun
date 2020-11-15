cb4e683551386489f6facdd2f5af6e464c46b552
Gary McGhee <gary@buzzware.com.au>
2020-11-15 20:12:20 +0800

    working now

---
+++ webapp/cloudrun-deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+GCP_PROJECT_ID=yourgarden
+CI_PROJECT_NAME=webapp
+docker build -t gcr.io/$GCP_PROJECT_ID/$CI_PROJECT_NAME:latest
+
+docker push gcr.io/$GCP_PROJECT_ID/$CI_PROJECT_NAME:latest
+#gcloud run services add-iam-policy-binding $CI_PROJECT_NAME --project $GCP_PROJECT_ID --member="allUsers" --role="roles/run.invoker" --region asia-northeast1
+gcloud run deploy $CI_PROJECT_NAME --image gcr.io/$GCP_PROJECT_ID/$CI_PROJECT_NAME:latest --platform managed --allow-unauthenticated --region asia-northeast1
